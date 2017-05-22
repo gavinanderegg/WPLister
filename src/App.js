e852ff367be25b3fa1ea91c5ac39d9a61c860dda
Gavin Anderegg <gavin@anderegg.ca>
2017-05-21 23:19:06 -0300

    Starting

--- src/App.js
+++ src/App.js
@@ -4,18 +4,59 @@ import './App.css';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+
+		this.state = {
+			searchText: '',
+			posts: [],
+			error: "asdfads",
+		};
+
+		this._searchTextChange = this._searchTextChange.bind(this);
+		this._handleSearch = this._handleSearch.bind(this);
+	}
+
+	_searchTextChange(event) {
+		this.setState({
+			searchText: event.target.value
+		});
+	}
+
+	_handleSearch() {
+		var currentText = this.state.searchText;
+
+		// https://public-api.wordpress.com/rest/v1.1/sites/apidemo.wordpress.com/posts/?number=1
+		// (https:\/\/)?(.*?\.wordpress.com).*
+
+		this.setState({
+			text: currentText
+		})
 	}
 
 	render() {
+		var message;
+
+		if (this.state.error !== false) {
+			message = this.state.error;
+		} else {
+			message = this.state.text;
+		}
+
+		// var namesList = names.map(function(name) {
+		// 	return <li>{name}</li>;
+		// });
+		// return <ul>{ namesList }</ul>
+
 		return (
 			<div className="App">
 				<div className="App-header">
 					<h2>WordPress.com Lister</h2>
 				</div>
 				<div className="App-form">
-					<input type="text" className="App-url"/>
-					<button className="App-load">Load</button>
+					<input type="text" className="App-url" onChange={this._searchTextChange} />
+					<button className="App-load" onClick={this._handleSearch}>Load</button>
+				</div>
+				<div>
+					{ message }
 				</div>
 			</div>
 		);
